8bf49143c84eb8368baabc8e9347eba3c70b58d6
Super Genius <ken@gnus.ai>
2022-12-30 21:25:31 -0800

    further debugging

--- src/restoreMultiImpl.ts
+++ src/restoreMultiImpl.ts
@@ -29,7 +29,7 @@ async function restoreMultiImpl(
         stateProvider.setState(State.CachePrimaryKey, primaryKeys);
 
         const pathString = core.getInput("paths");
-        core.debug(`Paths: ${pathString}`);
+        core.debug(`Paths: ${pathString.toString()}`);
 
         const multiPrimaryKeys = stringToArray(primaryKeys);
         const multiRestoreKeys = utils.getInputAsArrayOfArray(MultiInputs.RestoreKeys);
